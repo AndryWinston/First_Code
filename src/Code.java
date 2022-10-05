7453b9d86deede51de0e658b4d0947d232f59799
AndryWinston <alexkilla1@yandex.ru>
2022-10-05 17:19:57 +0300

    Hello World

---
+++ src/Code.java
@@ -0,0 +1,16 @@
+
+
+public class Code {
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu() {
+        System.out.println("Программа криптоанализа");
+        System.out.println("Инициализация...");
+        System.out.println("Что вы желаете сделать?");
+        System.out.println("1 - Шифрование текста");
+        System.out.println("2 - Расшифровка текста");
+        System.out.println("3 - Криптоанализ методом brute force");
+        System.out.println("4 - Выход из программы.");
+    }
+}
