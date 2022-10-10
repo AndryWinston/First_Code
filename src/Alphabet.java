d1db6131b7b2a4df0c89c64c30c8cb86643f416b
AndryWinston <alexkilla1@yandex.ru>
2022-10-10 20:28:09 +0300

    No matter

---
+++ src/Alphabet.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Alphabet {
+    public List<String> bigAphabet = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    public List<String> littleAphabet = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    public List<Character> punctuationMarks = Arrays.asList('?', '!', '"', '-', ' ', ':', '.', ',');
+}
--- src/Code.java
+++ src/Code.java
@@ -1,16 +1,13 @@
 
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 public class Code {
-    public static void main(String[] args) {
-        menu();
-    }
-    public static void menu() {
-        System.out.println("Программа криптоанализа");
-        System.out.println("Инициализация...");
-        System.out.println("Что вы желаете сделать?");
-        System.out.println("1 - Шифрование текста");
-        System.out.println("2 - Расшифровка текста");
-        System.out.println("3 - Криптоанализ методом brute force");
-        System.out.println("4 - Выход из программы.");
+    static Scanner scanner = new Scanner(System.in);
+    static Alphabet alphabet = new Alphabet();
+
+    public static void main(String[] args){
+        int key = scanner.nextInt();
+
     }
 }
