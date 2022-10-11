53a1d3c6b9c723ae6ba0312b4f40e4750416ac9f
AndryWinston <alexkilla1@yandex.ru>
2022-10-11 21:18:35 +0300

    Add score

--- src/Code.java
+++ src/Code.java
@@ -1,13 +1,23 @@
 
-import java.io.FileInputStream;
+
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Code {
     static Scanner scanner = new Scanner(System.in);
+    static Logica logica = new Logica();
     static Alphabet alphabet = new Alphabet();
+    private static final List<Character> chars = new ArrayList<>();
+    private final List<Character> newText = new ArrayList<>();
+    int key = scanner.nextInt();
 
-    public static void main(String[] args){
-        int key = scanner.nextInt();
+
+
+    public static void main(String[] args) {
+        char[] text = new char[logica.getScore];
+
 
     }
 }
--- src/Logica.java
+++ src/Logica.java
@@ -1,8 +1,27 @@
-public class Logica {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Logica {
+        private static final List<Character> chars = new ArrayList<>();
+        public int getScore;
+
+        public static int getScore() {
+                String fileName = "E:\\Programs\\Projects\\Caesar\\Code\\text.txt";
+                try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+                        int i;
+                        while ((i = reader.read()) != -1) {
+                                chars.add((char) i);
+                        }
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+                return chars.size();
+        }
 }
-//Принимать текст из файла,
-// считывать количество символов из файла,
+
 // затем помещать каждый символ в массив с размером = количеству символов,
 // затем перезаписывать в новый массив сравнивая с алфавитом,с размером = количеству символов
 // записывать в writer и создавать новый файл
